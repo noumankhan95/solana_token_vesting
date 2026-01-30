43328f01b41ecd82b63212352cf7eb64438fb949
noumankhan95 <noumankhan95@yahoo.com>
2026-01-30 12:10:06 +0500

    added base instructions

--- programs/vesting_app/src/lib.rs
+++ programs/vesting_app/src/lib.rs
@@ -1,18 +1,27 @@
 use anchor_lang::prelude::*;
-mod state;
-mod instructions;
 mod error;
+mod instructions;
+mod state;
 declare_id!("FD9zYpxBF4s7sEWqakb3RHngswJd2DKVr7HEXq1QBndb");
 
 #[program]
 pub mod vesting_app {
+    pub use super::instructions::*;
     use super::*;
+    pub fn initialize(
+        ctx: Context<LockTokens>,
+        amount: u64,
+        start_time: i64,
+        end_time: i64,
+        cliff_time: i64,
+        receiver: Pubkey,
+    ) -> Result<()> {
+        initialize_vesting(ctx, amount, start_time, end_time, cliff_time, receiver)?;
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init_unlock_tokens(ctx: Context<UnlockTokens>) -> Result<()> {
+        unlock_tokens(ctx)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
