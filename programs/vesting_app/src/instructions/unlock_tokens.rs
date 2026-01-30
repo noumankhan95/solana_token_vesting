34112102a671b94c4b55ef36fb379165847c085d
noumankhan95 <noumankhan95@yahoo.com>
2026-01-30 12:07:45 +0500

    fixing minor bugs

--- programs/vesting_app/src/instructions/unlock_tokens.rs
+++ programs/vesting_app/src/instructions/unlock_tokens.rs
@@ -9,10 +9,10 @@ use crate::state::Vesting;
 
 #[derive(Accounts)]
 pub struct UnlockTokens<'info> {
-    #[account(mut)]
+    #[account(mut,constraint=signer.key()==vesting_account.receiver)]
     pub signer: Signer<'info>,
 
-    #[account(mut,seeds=[b"vesting",vesting_account.receiver.key().as_ref(),vesting_account.token_mint.key().as_ref()],bump=vesting_account.bump)]
+    #[account(mut,seeds=[b"vesting",vesting_account.receiver.as_ref(),vesting_account.token_mint.as_ref()],bump=vesting_account.bump)]
     pub vesting_account: Account<'info, Vesting>,
     #[account(mut,associated_token::mint=vesting_account.token_mint,associated_token::authority=vesting_account)]
     pub vesting_token_account: InterfaceAccount<'info, TokenAccount>,
@@ -25,63 +25,42 @@ pub struct UnlockTokens<'info> {
 }
 
 pub fn unlock_tokens(ctx: Context<UnlockTokens>) -> Result<()> {
-    let vesting_account = &ctx.accounts.vesting_account;
-    if Clock::get()?.unix_timestamp < vesting_account.start_time {
+    let vesting = &mut ctx.accounts.vesting_account;
+    let now = Clock::get()?.unix_timestamp;
+
+    if Clock::get()?.unix_timestamp < vesting.cliff_time {
         return Ok(());
     }
 
-    if Clock::get()?.unix_timestamp > vesting_account.end_time {
-        let transfer_accounts = Transfer {
-            from: ctx.accounts.vesting_token_account.to_account_info(),
-            authority: ctx.accounts.vesting_account.to_account_info(),
-            to: ctx.accounts.receiver_token_account.to_account_info(),
-        };
-        let vesting_acc_key = vesting_account.receiver.key();
-        let vesting_acc_token = vesting_account.token_mint.key();
-        let signer_seeds: &[&[&[u8]]] = &[&[
-            b"vesting",
-            vesting_acc_key.as_ref(),
-            vesting_acc_token.as_ref(),
-            &[vesting_account.bump],
-        ]];
-        let cpi_ctx = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            transfer_accounts,
-        )
-        .with_signer(signer_seeds);
-        let amount = vesting_account
-            .amount
-            .saturating_sub(vesting_account.released);
-        token::transfer(cpi_ctx, amount)?;
+    let total_vested = if now >= vesting.end_time {
+        vesting.amount
     } else {
-        let now = Clock::get()?.unix_timestamp;
-        let elapsed = now - vesting_account.start_time;
-        let duration = vesting_account
-            .end_time
-            .saturating_sub(vesting_account.start_time);
-        let vested = (vesting_account.amount as u128 * elapsed as u128 / duration as u128) as u64;
-        let transfer_accounts = Transfer {
+        let elapsed = now - vesting.start_time;
+        let duration = vesting.end_time - vesting.start_time;
+
+        (vesting.amount as u128 * elapsed as u128 / duration as u128) as u64
+    };
+    let claimable = total_vested.saturating_sub(vesting.released);
+    let seeds: &[&[&[u8]]] = &[&[
+        b"vesting",
+        vesting.receiver.as_ref(),
+        vesting.token_mint.as_ref(),
+        &[vesting.bump],
+    ]];
+
+    let cpi_ctx = CpiContext::new(
+        ctx.accounts.token_program.to_account_info(),
+        Transfer {
             from: ctx.accounts.vesting_token_account.to_account_info(),
-            authority: ctx.accounts.vesting_account.to_account_info(),
             to: ctx.accounts.receiver_token_account.to_account_info(),
-        };
-        let vesting_acc_key = vesting_account.receiver.key();
-        let vesting_acc_token = vesting_account.token_mint.key();
-        let signer_seeds: &[&[&[u8]]] = &[&[
-            b"vesting",
-            vesting_acc_key.as_ref(),
-            vesting_acc_token.as_ref(),
-            &[vesting_account.bump],
-        ]];
-        let cpi_ctx = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            transfer_accounts,
-        )
-        .with_signer(signer_seeds);
-        let amount = vesting_account
-            .amount
-            .saturating_sub(vesting_account.released);
-        token::transfer(cpi_ctx, amount)?;
-    }
+            authority: vesting.to_account_info(),
+        },
+    )
+    .with_signer(seeds);
+
+    token::transfer(cpi_ctx, claimable)?;
+
+    // 4. Update state
+    vesting.released += claimable;
     Ok(())
 }
